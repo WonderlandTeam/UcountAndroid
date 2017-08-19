45e38c26e34d3429c0969e1a6eb68f4e5b72b5b1
sweetstreet <942290857@qq.com>
2017-08-19 10:45:03 +0800

    引导页跳转入登录界面

--- UCount_Android/app/src/main/java/team/wonderland/ucount/ucount_android/activity/MainActivity.java
+++ UCount_Android/app/src/main/java/team/wonderland/ucount/ucount_android/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package team.wonderland.ucount.ucount_android.activity;
 
+import android.content.Intent;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -17,10 +19,12 @@ import team.wonderland.ucount.ucount_android.R;
 public class MainActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
+    private Button enterButton;
     private int[] mImgIds = new int[]{
             R.drawable.guide_image1,R.drawable.guide_image2,R.drawable.guide_image3
     };
     private List<ImageView> mImages = new ArrayList<ImageView>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mViewPager = (ViewPager)findViewById(R.id.id_viewPager);
         //为viewPager设置动画效果
-        //mViewPager.setPageTransformer(true,new DepthPageTransformer());
+        mViewPager.setPageTransformer(true,new DepthPageTransformer());
         mViewPager.setAdapter(new PagerAdapter(){
             @Override
             public Object instantiateItem(ViewGroup container, int position){
@@ -57,5 +61,14 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        enterButton = (Button)findViewById(R.id.bt_enter);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
