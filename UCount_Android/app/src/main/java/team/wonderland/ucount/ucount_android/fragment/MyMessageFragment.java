786fe701ba8cbf34d2a462397e2cb9bbf7fce909
Lili0820 <1179028267@qq.com>
2017-08-31 14:34:33 +0800

    下拉加载组件

--- UCount_Android/app/src/main/java/team/wonderland/ucount/ucount_android/fragment/MyMessageFragment.java
+++ UCount_Android/app/src/main/java/team/wonderland/ucount/ucount_android/fragment/MyMessageFragment.java
@@ -6,15 +6,26 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import team.wonderland.ucount.ucount_android.R;
+import uk.co.imallan.jellyrefresh.JellyRefreshLayout;
+import uk.co.imallan.jellyrefresh.PullToRefreshLayout;
 
 /**
  * 我的消息
  * Created by CLL on 17/8/30.
  */
 public class MyMessageFragment extends Fragment {
+    private JellyRefreshLayout jellyRefreshLayout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view=inflater.inflate(R.layout.my_message_fragment,container,false);
+        jellyRefreshLayout=view.findViewById(R.id.message_loading);
+        jellyRefreshLayout.setPullToRefreshListener(new PullToRefreshLayout.PullToRefreshListener() {
+            @Override
+            public void onRefresh(PullToRefreshLayout pullToRefreshLayout) {
+
+            }
+        });
+
         return view;
     }
 }
