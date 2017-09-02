2f3b6e035343b2b7e0ad4af72a5c25d5740d79d7
Lili0820 <1179028267@qq.com>
2017-09-02 09:02:49 +0800

    三个报表框架

--- UCount_Android/app/src/main/java/team/wonderland/ucount/ucount_android/fragment/ReportFragment.java
+++ UCount_Android/app/src/main/java/team/wonderland/ucount/ucount_android/fragment/ReportFragment.java
@@ -2,13 +2,13 @@ package team.wonderland.ucount.ucount_android.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.gigamole.navigationtabstrip.NavigationTabStrip;
+import team.wonderland.ucount.ucount_android.Adapter.FragAdapter;
 import team.wonderland.ucount.ucount_android.R;
 
 import java.util.ArrayList;
@@ -21,10 +21,12 @@ import java.util.List;
 public class ReportFragment extends Fragment {
     private ViewPager mViewPager;
     private NavigationTabStrip mTopNavigationTabStrip;
-    private List<View> viewList;
+    private List<Fragment> fragments;
+    private Fragment report1;
+    private Fragment report2;
+    private Fragment report3;
 
-    public  ReportFragment(){
-
+    public ReportFragment() {
     }
 
     @Override
@@ -36,42 +38,23 @@ public class ReportFragment extends Fragment {
     }
 
     private void initUI(View view) {
-        mViewPager = (ViewPager) view.findViewById(R.id.vp);
-        mTopNavigationTabStrip = (NavigationTabStrip) view.findViewById(R.id.nts_top);
+        mViewPager = (ViewPager) view.findViewById(R.id.report_vp);
+        mTopNavigationTabStrip = (NavigationTabStrip) view.findViewById(R.id.report_nts_top);
+        report1 = new ReportOneFragment();
+        report2 = new ReportTwoFragment();
+        report3 = new ReportTwoFragment();
     }
 
     private void setUI(LayoutInflater inflater, final View view) {
-        View view1 = inflater.inflate(R.layout.report_one, null);
-        View view2 = inflater.inflate(R.layout.report_two,null);
-        View view3 = inflater.inflate(R.layout.report_three, null);
-        viewList = new ArrayList<View>();// 将要分页显示的View装入数组中
-        viewList.add(view1);
-        viewList.add(view2);
-        viewList.add(view3);
-        mViewPager.setAdapter(new PagerAdapter() {
-            @Override
-            public int getCount() {
-                return viewList.size();
-            }
 
-            @Override
-            public boolean isViewFromObject(final View view, final Object object) {
-                return view == object;
-            }
+        fragments = new ArrayList<>();
+        fragments.add(report1);
+        fragments.add(report2);
+        fragments.add(report3);
 
-            @Override
-            public void destroyItem(final ViewGroup container, final int position, final Object object) {
-//                ((ViewPager) container).removeView((View) object);
-                container.removeView(viewList.get(position));
-            }
+        FragAdapter fragAdapter= new FragAdapter(getChildFragmentManager(), fragments);
+        mViewPager.setAdapter(fragAdapter);
 
-            @Override
-            public Object instantiateItem(final ViewGroup container, final int position) {
-                return viewList.get(position);
-            }
-        });
-
-        mTopNavigationTabStrip.setTabIndex(0, true);
         mTopNavigationTabStrip.setOnTabStripSelectedIndexListener(new NavigationTabStrip.OnTabStripSelectedIndexListener() {
             @Override
             public void onStartTabSelected(String title, int index) {
@@ -83,5 +66,8 @@ public class ReportFragment extends Fragment {
 
             }
         });
+        mTopNavigationTabStrip.setTabIndex(0, true);
+        mTopNavigationTabStrip.setViewPager(mViewPager);
     }
+
 }
---
+++ UCount_Android/app/src/main/java/team/wonderland/ucount/ucount_android/fragment/ReportTwoFragment.java
@@ -0,0 +1,19 @@
+package team.wonderland.ucount.ucount_android.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import team.wonderland.ucount.ucount_android.R;
+
+/**
+ * Created by CLL on 17/9/2.
+ */
+public class ReportTwoFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.report_two, container, false);
+        return view;
+    }
+}
