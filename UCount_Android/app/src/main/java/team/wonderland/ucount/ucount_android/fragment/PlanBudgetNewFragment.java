95ce4ffc2d685c61bc2a8c6af2d0001987de26e3
sweetstreet <942290857@qq.com>
2017-09-07 23:11:33 +0800

    新增预算添加了时间选择框

--- UCount_Android/app/src/main/java/team/wonderland/ucount/ucount_android/fragment/PlanBudgetNewFragment.java
+++ UCount_Android/app/src/main/java/team/wonderland/ucount/ucount_android/fragment/PlanBudgetNewFragment.java
@@ -2,6 +2,7 @@ package team.wonderland.ucount.ucount_android.fragment;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.UiThread;
 import android.support.v4.app.Fragment;
@@ -16,11 +17,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bigkoo.pickerview.TimePickerView;
+
 import at.markushi.ui.CircleButton;
 
 import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.EFragment;
 import org.androidannotations.rest.spring.annotations.RestService;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import team.wonderland.ucount.ucount_android.R;
 import team.wonderland.ucount.ucount_android.json.BudgetAddJson;
@@ -31,17 +37,19 @@ import team.wonderland.ucount.ucount_android.service.BudgetService;
  */
 @EFragment
 public class PlanBudgetNewFragment extends Fragment {
-    CircleButton save;
-    ImageView back;
-    CardView cv_type;
-    TextView tv_type;
-    EditText et_money;
-    CardView cv_money;
-    String[] titles = {"饮食", "日用", "水电气", "通讯网费", "电子设备", "交通", "衣帽鞋服", "护肤品",
+    private CircleButton save;
+    private ImageView back;
+    private CardView cv_type;
+    private CardView cv_money;
+    private CardView cv_date;
+    private TextView tv_type;
+    private EditText et_money;
+    private TextView tv_date;
+
+    private String[] titles = {"饮食", "日用", "水电气", "通讯网费", "电子设备", "交通", "衣帽鞋服", "护肤品",
             "彩妆", "首饰", "培训", "书", "文具", "图像影音", "组织活动","捐款","恋爱","社交","兴趣"};
-    String type;
-    private String username;
     private String consumeType;
+    private String username;
     private double consumeMoney;
     private String consumeTime;
 
@@ -58,6 +66,10 @@ public class PlanBudgetNewFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 //TODO 设定以下变量的值
+                //consumeMoney的获取方法：Double.parseDouble(cv_money.getText());
+                //consumeType的获取方法:tv_type.getText();
+                //consumeTime的获取方法:tv_date.getText();
+                //username的获取方法： getActivity().getSharedPreferences("userInfo",0).getString("USERNAME","");
                 username="";
                 consumeType="";
                 consumeMoney=0;
@@ -76,7 +88,6 @@ public class PlanBudgetNewFragment extends Fragment {
         });
 
         cv_type = view.findViewById(R.id.plan_budget_new_cardview_type);
-
         cv_type.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -86,8 +97,8 @@ public class PlanBudgetNewFragment extends Fragment {
                         .setSingleChoiceItems(titles, 0, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                type = titles[which];//根据which决定选择了哪一个子项
-                                tv_type.setText(type);
+                                consumeType = titles[which];//根据which决定选择了哪一个子项
+                                tv_type.setText(consumeType);
                                 dialog.dismiss();
                             }
                         }).create();
@@ -106,6 +117,37 @@ public class PlanBudgetNewFragment extends Fragment {
                 et_money.requestFocus();
             }
         });
+
+        cv_date = view.findViewById(R.id.plan_budget_new_cardview_date);
+        tv_date = view.findViewById(R.id.plan_budget_new_tv_date);
+        final Calendar startDate = Calendar.getInstance();
+        startDate.set(2017, 8, 1);
+        final Calendar endDate = Calendar.getInstance();
+        endDate.set(2037, 11, 31);
+        cv_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //时间选择器
+                final TimePickerView pvTime = new TimePickerView.Builder(getActivity(), new TimePickerView.OnTimeSelectListener() {
+                    @Override
+                    public void onTimeSelect(Date date, View v) {//选中事件回调
+                        tv_date.setText((date.getYear()+1900)+"-"+(date.getMonth()+1));
+                    }
+                })
+                        .setType(new boolean[]{true,true,false,false,false,false})
+                        .setLabel("年","月","","","","")
+                        .setRangDate(startDate, endDate)
+                        .setCancelText("取消")//取消按钮文字
+                        .setSubmitText("确认")//确认按钮文字
+                        .setCancelColor(getResources().getColor(R.color.text_green))
+                        .setSubmitColor(getResources().getColor(R.color.text_green))
+                        .setLineSpacingMultiplier(3f)
+                        .build();
+                pvTime.setDate(Calendar.getInstance());//注：根据需求来决定是否使用该方法（一般是精确到秒的情况），此项可以在弹出选择器的时候重新设置当前时间，避免在初始化之后由于时间已经设定，导致选中时间与当前时间不匹配的问题。
+                pvTime.show();
+            }
+        });
+
         return view;
     }
 
